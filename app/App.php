805c8324e25eaf511dfa5317785ec90f21b31f8d
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-06-07 14:52:49 +0300

    add file reading function

--- app/App.php
+++ app/App.php
@@ -3,3 +3,37 @@
 declare(strict_types = 1);
 
 // Your Code
+
+$files = scandir(FILES_PATH);
+
+// iterate over every file in the transaction_files
+foreach ($files as $file) {
+    // exclude default dir
+    if ($file !== '.' && $file !== '..') {
+        // take the current path of the current file in the loop
+        $fullPath = FILES_PATH . $file;
+
+        if (is_file($fullPath)) {
+
+            $csvFile = new SplFileObject($fullPath);
+            $csvFile->setFlags(SplFileObject::READ_CSV);
+
+            $headers = [];
+            $rows = [];
+
+            foreach ($csvFile as $index => $row) {
+                if ($csvFile->eof() || $row === [null]) continue;
+
+                if ($index === 0) {
+                    $headers = $row;
+                } else {
+                    $rows[] = array_combine($headers, $row);
+                }
+            }
+            echo "<pre>";
+            
+            print_r($rows);
+            echo "</pre>";
+        }
+    }
+}
--- public/index.php
+++ public/index.php
@@ -9,3 +9,8 @@ define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
 
 /* YOUR CODE (Instructions in README.md) */
+include(VIEWS_PATH . 'transactions.php');
+include(APP_PATH . 'App.php');
+
+
+
